f06017f2eb9dc0fe4039ad6ab78ec1a99311556c
Roman Franklin <romankfranklin213@icloud.com>
2020-10-21 11:59:37 -0400

    working on my Main.js

---
+++ app/screens/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+export default () => {
+  return (
+    <view>
+      <Text>lol</Text>
+      <StatusBar> style="auto"</StatusBar>
+    </view>
+  );
+};
